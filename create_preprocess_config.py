48071198991225d44ea50618f9ddb131342e6c52
michele1993 <michele.garibbo@gmail.com>
2025-04-15 17:12:47 +0200

    split scripts

--- create_preprocess_config.py
+++ create_preprocess_config.py
@@ -1,35 +1,10 @@
-import os
-import subprocess
-
-## Download some fasta sequence
-concat_path = "chr20_21_22.fa"
-
-if not os.path.exists(concat_path):
-    # Download the files
-    urls = [
-        "https://hgdownload.soe.ucsc.edu/goldenpath/hg38/chromosomes/chr20.fa.gz",
-        "https://hgdownload.soe.ucsc.edu/goldenpath/hg38/chromosomes/chr21.fa.gz",
-        "https://hgdownload.soe.ucsc.edu/goldenpath/hg38/chromosomes/chr22.fa.gz"
-    ]
-    
-    for url in urls:
-        subprocess.run(["wget", url], check=True)
-
-    # Decompress the files
-    for chr_num in ["20", "21", "22"]:
-        subprocess.run(["zcat", f"chr{chr_num}.fa.gz"], stdout=open(f"chr{chr_num}.fa", "w"), check=True)
-
-    # Concatenate the files
-    with open(concat_path, "w") as outfile:
-        for chr_num in ["20", "21", "22"]:
-            with open(f"chr{chr_num}.fa", "r") as infile:
-                outfile.write(infile.read())
-
-print("File creation complete:", concat_path)
-
 ## Create necessary `.yaml` config file
+
+fasta_file_name = "chr20_21_22.fa"
+
 full_fasta_path = os.path.abspath(concat_path)
 output_dir = os.path.abspath("preprocessed_data")
+
 output_yaml = f"""
 - datapaths: ["{full_fasta_path}"]
   output_dir: "{output_dir}"
